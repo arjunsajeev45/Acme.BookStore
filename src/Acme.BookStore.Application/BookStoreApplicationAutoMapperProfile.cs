df7620b25508dea60039e654dfd077ad431e8366
speehive <speehive@DESKTOP-TPF0JOE>
2023-01-30 10:50:19 +0530

    Add project files.

---
+++ src/Acme.BookStore.Application/BookStoreApplicationAutoMapperProfile.cs
@@ -0,0 +1,15 @@
+﻿using Acme.BookStore.Books;
+using AutoMapper;
+
+namespace Acme.BookStore;
+
+public class BookStoreApplicationAutoMapperProfile : Profile
+{
+    public BookStoreApplicationAutoMapperProfile()
+    {
+        CreateMap<Book, BookDto>().ReverseMap();
+       // CreateMap<BookDto, Book>();
+        CreateMap<CreateUpdateBookDto, Book>();
+    }
+}
+
---
+++ src/Acme.BookStore.Application/Books/BookAppService.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading.Tasks;
+using Volo.Abp.Application.Dtos;
+using Volo.Abp.Application.Services;
+using Volo.Abp.Domain.Repositories;
+
+namespace Acme.BookStore.Books;
+
+public class BookAppService :
+    CrudAppService<
+        Book, //The Book entity
+        BookDto, //Used to show books
+        Guid, //Primary key of the book entity
+        PagedAndSortedResultRequestDto, //Used for paging/sorting
+        CreateUpdateBookDto>, //Used to create/update a book
+    IBookAppService //implement the IBookAppService
+{
+
+    IRepository<Book, Guid> repository;
+    public BookAppService(IRepository<Book, Guid> repository)
+        : base(repository)
+    {
+        this.repository = repository;
+    }
+
+    public override Task<BookDto> CreateAsync(CreateUpdateBookDto input)
+    {
+        input.Name = input.Name + "Speehive";
+
+        return base.CreateAsync(input);
+    }
+
+
+
+    public void SomeLogic(BookDto input, string s)
+    {
+        var book1=ObjectMapper.Map<BookDto,Book>(input);
+
+        Book book = new Book();
+        book.Name = input.Name; 
+        book.Type= input.Type;
+
+
+        repository.InsertAsync(book1);
+    }
+
+    [HttpGet]
+    public void Hello()
+    {
+
+    }
+}
+
